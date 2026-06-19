a559231a7b294e347bd56f0ed2ae30387a486ef1
uwu <uwu@lycheegirl.dev>
2026-06-19 18:29:35 +0200

    Create tusb_config.h

---
+++ platform_pico/tusb_config.h
@@ -0,0 +1,8 @@
+#ifndef TUSB_CONFIG_H
+#define TUSB_CONFIG_H
+
+#define CFG_TUSB_RHPORT0_MODE       OPT_MODE_DEVICE
+#define CFG_TUD_HID                 1 // Enable HID
+#define CFG_TUD_ENABLED             1
+
+#endif
